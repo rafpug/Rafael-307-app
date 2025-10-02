60a1c86d4f3b3e895dc408adaecc327564fd1779
raf <rafaelsalcedoprado@gmail.com>
2025-10-01 23:49:42 -0700

    Finished part 6 of adding a backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,6 +44,14 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -53,6 +61,22 @@ app.get("/users", (req, res) => {
   } else {
     res.send(users);
   }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
