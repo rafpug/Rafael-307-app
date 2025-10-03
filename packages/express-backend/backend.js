8cfa64d381b697dedca8d1814310cfff310fcb6a
raf <rafaelsalcedoprado@gmail.com>
2025-10-02 22:16:52 -0700

    Feat: Add Cross-Origin Resource Sharing

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,12 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 const users = {
   users_list: [
